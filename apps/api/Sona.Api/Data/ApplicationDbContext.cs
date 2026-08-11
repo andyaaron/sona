c5df0708b43a48b4c390edc0d9832b30e7f6d0f3
Kenton Sallee <Kenton.Sallee@hcahealthcare.com>
2026-08-11 15:33:11 -0400

    Entity Framework w/Keyvault and Serilog prepped.

---
+++ apps/api/Sona.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sona.Api.Data.DbModels;
+
+namespace Sona.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+
+        public DbSet<AppLog> AppLogs { get; set; }
+    }
+}
