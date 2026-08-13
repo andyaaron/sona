50f03312420f062091eaccffc3185286d55d1bb7
hvu6110 <hvu6110@hca.corpad.net>
2026-08-13 15:05:09 -0400

    setup admin/api to run from launch command

--- apps/admin/vite.config.ts
+++ apps/admin/vite.config.ts
@@ -1,8 +1,45 @@
 import tailwindcss from '@tailwindcss/vite'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import react from '@vitejs/plugin-react'
+import child_process from 'node:child_process'
+import fs from 'node:fs'
 import path from 'node:path'
+import { env } from 'node:process'
 import { defineConfig } from 'vite'
+
+const baseFolder =
+  env.APPDATA !== undefined && env.APPDATA !== ''
+    ? `${env.APPDATA}/ASP.NET/https`
+    : `${env.HOME}/.aspnet/https`
+
+const certificateName = 'admin'
+const certFilePath = path.join(baseFolder, `${certificateName}.pem`)
+const keyFilePath = path.join(baseFolder, `${certificateName}.key`)
+
+if (!fs.existsSync(baseFolder)) {
+  fs.mkdirSync(baseFolder, { recursive: true })
+}
+
+if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
+  if (
+    0 !==
+    child_process.spawnSync(
+      'dotnet',
+      [
+        'dev-certs',
+        'https',
+        '--export-path',
+        certFilePath,
+        '--format',
+        'Pem',
+        '--no-password',
+      ],
+      { stdio: 'inherit' },
+    ).status
+  ) {
+    throw new Error('Could not create certificate.')
+  }
+}
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -20,6 +57,13 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+  server: {
+    port: 5173,
+    https: {
+      key: fs.readFileSync(keyFilePath),
+      cert: fs.readFileSync(certFilePath),
     },
   },
 })
